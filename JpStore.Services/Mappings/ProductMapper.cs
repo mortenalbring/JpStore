31e115e991030f6d77c90385b9578d14583d0525
Jaypal Raj <jaypalraj@hotmail.com>
2014-05-19 23:37:28 +0100

    commit1902

---
+++ JpStore.Services/Mappings/ProductMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using JpStore.Model.Products;
+using JpStore.Services.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JpStore.Services.Mappings
+{
+    public static class ProductMapper
+    {
+        public static ProductView ConvertToProductView(this Product product)
+        {
+            return Mapper.Map<Product, ProductView>(product);
+        }
+    }
+}
---
+++ JpStore.Services/ViewModels/ProductView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JpStore.Services.ViewModels
+{
+    public class ProductView
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public int BrandId { get; set; }
+        public string ProductTitle { get; set; }
+        public string Category { get; set; }
+        public string Brand { get; set; }
+    }
+}
